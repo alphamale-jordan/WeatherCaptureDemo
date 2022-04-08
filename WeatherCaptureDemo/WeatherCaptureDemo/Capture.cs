e99470f3b1bf682cf7eff93f5c2705f94eda097d
Oscar Mntonintshi <81410334+alphamale-jordan@users.noreply.github.com>
2022-04-08 21:31:11 +0200

    updated commit

---
+++ WeatherCaptureDemo/WeatherCaptureDemo/Capture.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LogInDemo
+{
+    public partial class Capture : Form 
+    {
+        public Capture()
+        {
+            InitializeComponent();
+        }
+        
+               
+        
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+       {
+
+       }
+
+        private void label2_Click(object sender, EventArgs e)
+       {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+       }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       private void label3_Click(object sender, EventArgs e)
+        {
+
+       }
+    
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+       }
+
+      private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+      {
+
+      
+  }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+           
+            // string path = @"C:\\Users\\Alex Moyo\\source\\repos\\demo Capture\\demo Capture\\DEMO FILE\\demo.txt";
+           string path = @"C:\\Users\\Alex Moyo\\source\\repos\\WeatherCaptureDemo\\WeatherCaptureDemo\\Weather file\\weather.txt";
+            //using System.IO; grant us permission to use this streamwriter.
+            using (StreamWriter alex = new StreamWriter(path, true))
+            {
+               
+                    alex.Write("\n" + "  " + cbCity.Text + ",");
+                    alex.Write(" " + Date + ",");
+                    alex.Write(" " + txtMin.Text + "°C" + ",");
+                    alex.Write(" " + txtMax.Text + "°C" + ",");
+                    alex.Write(" " + cbPrec.Text +"%" +",");
+                    alex.Write(" " + txtHumi.Text + "%" + ",");
+                    alex.Write(" " + txtSpeed.Text + " km/h"+ "\n");
+
+                    MessageBox.Show("Details have been captured successfuly");
+                
+              
+                 
+            }
+           
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            string pat = @"C:\\Users\\Alex Moyo\\source\\repos\\WeatherCaptureDemo\\WeatherCaptureDemo\\Weather file\\weather.txt";
+            var str = File.ReadAllText(pat);
+            richTextBox1.Text = str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void date_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            Find Search = new Find();
+            Search.ShowDialog();
+        }
+    }
+}
---
+++ WeatherCaptureDemo/WeatherCaptureDemo/DemoCaptureF.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WeatherCaptureDemo
+{
+    partial class DemoCaptureF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtPrec = new System.Windows.Forms.TextBox();
+            this.txtHumi = new System.Windows.Forms.TextBox();
+            this.txtSpeed = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.Date = new System.Windows.Forms.DateTimePicker();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.cbCity = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Minimum Temp";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Maximum Temp";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precipitation";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Humidity";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Wind Speed";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(131, 75);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(121, 20);
+            this.txtMin.TabIndex = 9;
+            this.txtMin.TextChanged += new System.EventHandler(this.txtMin_TextChanged);
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(131, 101);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(121, 20);
+            this.txtMax.TabIndex = 10;
+            // 
+            // txtPrec
+            // 
+            this.txtPrec.Location = new System.Drawing.Point(131, 127);
+            this.txtPrec.Name = "txtPrec";
+            this.txtPrec.Size = new System.Drawing.Size(121, 20);
+            this.txtPrec.TabIndex = 11;
+            // 
+            // txtHumi
+            // 
+            this.txtHumi.Location = new System.Drawing.Point(131, 153);
+            this.txtHumi.Name = "txtHumi";
+            this.txtHumi.Size = new System.Drawing.Size(121, 20);
+            this.txtHumi.TabIndex = 12;
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Location = new System.Drawing.Point(131, 179);
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(121, 20);
+            this.txtSpeed.TabIndex = 13;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(298, 18);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(308, 96);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCapture.Location = new System.Drawing.Point(58, 220);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(149, 23);
+            this.btnCapture.TabIndex = 15;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // Date
+            // 
+            this.Date.Location = new System.Drawing.Point(131, 46);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(121, 20);
+            this.Date.TabIndex = 16;
+            // 
+            // btnView
+            // 
+            this.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnView.Location = new System.Drawing.Point(298, 124);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(103, 23);
+            this.btnView.TabIndex = 17;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Location = new System.Drawing.Point(496, 124);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(110, 23);
+            this.btnSearch.TabIndex = 18;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(528, 220);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Close";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // cbCity
+            // 
+            this.cbCity.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbCity.FormattingEnabled = true;
+            this.cbCity.Items.AddRange(new object[] {
+            "Durban",
+            "Cape Town",
+            "Johannesburg",
+            "Pretoria",
+            "Polokwane",
+            "Port Elizabeth",
+            "Tzaneen",
+            "Mbombela",
+            "Mafikeng",
+            "Kimberly"});
+            this.cbCity.Location = new System.Drawing.Point(131, 17);
+            this.cbCity.Name = "cbCity";
+            this.cbCity.Size = new System.Drawing.Size(121, 21);
+            this.cbCity.TabIndex = 21;
+            // 
+            // DemoCaptureF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 262);
+            this.Controls.Add(this.cbCity);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.txtHumi);
+            this.Controls.Add(this.txtPrec);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DemoCaptureF";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DemoCaptureF";
+            this.Load += new System.EventHandler(this.DemoCaptureF_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtPrec;
+        private System.Windows.Forms.TextBox txtHumi;
+        private System.Windows.Forms.TextBox txtSpeed;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.DateTimePicker Date;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox cbCity;
+    }
+}
